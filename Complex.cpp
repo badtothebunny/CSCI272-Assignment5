9de668a0cdf8fba4e515528286d5fc4f5d992833
badtothebunny <maria.m.psomas@gmail.com>
2025-09-13 10:08:47 -0400

    Add files via upload

---
+++ Complex.cpp
@@ -0,0 +1,74 @@
+#include "Complex.h"
+//Complex class implementation section.
+//Programmed by Maria Psomas
+Complex::Complex()
+{
+	real = 0;
+	imaginary = 0;
+}
+Complex::Complex(float r, float i)
+{
+	real = r;
+	imaginary = i;
+}
+void Complex::getComplex()
+{
+	cout<<"Enter real number and imaginary number: "<<endl;
+	cin>>real>>imaginary;
+}
+void Complex::setComplex(float rl, float iy)
+{
+	real = rl;
+	imaginary = iy;
+}
+void Complex::displayComplex()
+{
+	cout<<"Complex number: \t"<<real<<"+i"<<imaginary<<endl;
+}
+float Complex::getReal()
+{
+	return real;
+}
+float Complex::getImaginary()
+{
+	return imaginary;
+}
+Complex Complex::operator+(Complex &add)
+{
+	Complex ans;
+	ans.real = real + add.real;
+	ans.imaginary = imaginary + add.imaginary;
+	return ans;
+}
+Complex Complex::operator-(Complex &sub)
+{
+	Complex ans;
+	ans.real = sub.real - real;
+	ans.imaginary = sub.imaginary - imaginary;
+}
+Complex Complex::operator*(Complex &mul)
+{
+	Complex ans;
+	ans.real = (real * mul.real) - (imaginary * mul.imaginary);
+	ans.imaginary = (real * mul.imaginary) + (imaginary * mul.real);
+	return ans;
+}
+Complex Complex::operator/(Complex &div)
+{
+	Complex ans;
+	ans.real = ((real * div.real)+(imaginary * imaginary))/((div.real * div.real)-(div.imaginary * div.imaginary));
+	ans.imaginary = ((imaginary * div.real)-(real * div.imaginary))/((div.real * div.real)-(div.imaginary * div.imaginary));
+	return ans;
+}
+void Complex::operator=(Complex &eql)
+{
+	real = eql.real;
+	imaginary = eql.imaginary;
+}
+bool Complex::operator==(Complex &equ)
+{
+	if (real == equ.real && imaginary == equ.imaginary)
+		return true;
+	else
+		return false;
+}
---
+++ main 10.4.1.cpp
@@ -0,0 +1,37 @@
+//This project obtains complex numbers from the user and manipulates them.
+//Programmed by Maria Psomas 
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Complex.h"
+using namespace std;
+int main(int argc, char** argv) {
+	Complex num,val,ans;
+	string inp;//Operations the user enters will be char type for if statements.
+	cout<<"Welcome. This program is a complex number calculator."<<endl;
+	while (inp != "!")
+	{
+		cout<<"Please enter two complex numbers."<<endl;
+		num.getComplex();
+		val.getComplex();
+		cout<<"Enter operation:(! terminates)"<<endl;
+		cin>>inp;
+		if (inp == "+")//for addition
+			(num+val).displayComplex();
+		else if (inp == "-")//for subtraction
+			(num-val).displayComplex();
+		else if (inp == "*" || inp == "x")//For multiplication
+		//User might type x instead of *, so I account for that
+			(num*val).displayComplex();
+		else if (inp == "/") //For division
+			(num/val).displayComplex();
+		else if (inp == "==")
+			if (num == val)
+				cout<<"They are equal.";
+			else 
+				cout<<"They are not equal.";
+		cout<<endl;
+	}
+	cout<<"Thank you for using this program."<<endl;
+	return 0;
+}
